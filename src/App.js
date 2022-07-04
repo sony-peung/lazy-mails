b73decc986d753174760f9bd96daf2b41a1f5534
Sony Peung <sonypeung@Sony-Peung.local>
2022-07-04 15:26:06 +0200

    added latest changes

--- src/App.js
+++ src/App.js
@@ -16,17 +16,16 @@ function LazyMails() {
     )
 }
 
-function Card(props) {
+function Card({ item }) {
+    const { price, title, properties } = item;
+
     return (
-        <li className={`${styles.card} ${props.extraClass}`}>
-            <h3>Gmail</h3>
+        <li className={styles.card}>
+            <h3>{title}</h3>
             <ul className={styles.card_items}>
-                <li>Instant delivery</li>
-                <li>Warmed</li>
-                <li>All Regions</li>
-                <li>Phone Verified</li>
+                { properties.map( elem => <li key={elem}>{elem}</li>)}
             </ul>
-            <p className={styles.price}>$0.00</p>
+            <p className={styles.price}>{price}</p>
             <a href='https://discord.gg/phPNvUfFVX'><button className={styles.purchase_btn}>Purchase</button></a>
         </li>
     )
@@ -63,7 +62,7 @@ function App() {
               </li>
           </ul>
       </header>
-        <body  className={styles.app_body}>
+        <div className={styles.app_body}>
             <div className={styles.intro}>
                 <h2>Welcome to Lazy Mails</h2>
                 <h1>The <span className={styles.orange}>MOST</span> Reliable Mails <span className={styles.orange}>EVER</span></h1>
@@ -89,14 +88,61 @@ function App() {
                         },
                     }}
                 >
-                    <SwiperSlide><Card /></SwiperSlide>
-                    <SwiperSlide><Card /></SwiperSlide>
-                    <SwiperSlide><Card /></SwiperSlide>
-                    <SwiperSlide><Card /></SwiperSlide>
-                    <SwiperSlide><Card /></SwiperSlide>
+                    <SwiperSlide>
+                        <Card
+                            item={
+                                {
+                                    price: '$0.00',
+                                    title: 'Forwarded Gmails',
+                                    properties: [
+                                        'High Quality Gmails',
+                                        'Phone verified',
+                                        'Forwarded to an aged Mastermail',
+                                        'Perfect for Raffles & VeVe',
+                                        'Anti Spam Filter',
+                                        'Firstname/Lastname included',
+                                        'Delivered as CSV File or Excel File',
+                                        '30 Days Warranty'
+                                    ]
+                                }}/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Card
+                            item={
+                                {
+                                    price: '$0.00',
+                                    title: 'Nike/SNKRS Accounts',
+                                    properties: [
+                                        'Verified with a HQ phone number',
+                                        'Choose between Hotmail/Web.de or Gmail',
+                                        'Choose the region for the Nike Account',
+                                        'Firstname, Lastname & Birthday included',
+                                        'Nike login & Email login included',
+                                        'Delivered as CSV File',
+                                        'Delivery within 24h',
+                                        'Lifetime Warranty (1x Warranty)'
+                                    ]}
+                            }/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Card item={
+                        {
+                            price: '$0.00',
+                            title: 'Forwarded Web.de Mails',
+                            properties: [
+                                'Zero Clips, No Delay, No Spam filter',
+                                'Forwarded to a Web, GMail or Catchall',
+                                'Forwarded in packs of 100',
+                                'Firstname/Lastname included',
+                                'Cheap Mails perfect for all kinds of raffles',
+                                'Delivered as CSV File',
+                                '30 Days Warranty'
+                            ]
+                        }}/>
+                    </SwiperSlide>
                 </Swiper>
             </div>
-        </body>
+        </div>
       <footer className={styles.app_footer}>
           <ul className={styles.footerWrapper}>
               <li className={styles.leftFooter}>
